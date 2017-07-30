2e2bca1013e850f50520201ada493f476c0947e1
Peter Smatana <petersmatana@gmail.com>
2017-07-30 10:25:17 +0200

    working

--- dir1/dir2/jee_here.py
+++ dir1/dir2/jee_here.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from run import application
+from flask_restful import Resource
+from run import api
 
-
+'''
 @application.route('/nefunguje')
 def not_working():
     return jsonify({'api': 'fail'})
+'''
 
 
-def omg():
-    print('hello')
+class DalsiPokus(Resource):
 
+    def get(self):
+        return {'api': 'ok'}
+
+api.add_resource(DalsiPokus, '/dalsi_pokus')
 
--- run.py
+++ run.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify
+from flask_restful import Api
 
+# from dir1.dir2.jee_here import DalsiPokus
 
 application = Flask(__name__)
+api = Api(application)
 
-
+'''
 @application.route('/funguje')
 def working():
     return jsonify({'api': 'ok'})
+'''
 
 if __name__ == '__main__':
     application.run(port=5000, debug=True)
